b9f22e3ad04d47e36eb16edcff002a9255491b22
seansameng <127754501+seansameng@users.noreply.github.com>
2025-12-15 10:27:00 +0700

    Initial project setup
    Initial project setup

---
+++ src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/EcommerceGraduationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.stepacademy.sameng.ecommerce_graduation_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceGraduationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceGraduationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stepacademy/sameng/ecommerce_graduation_project/EcommerceGraduationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepacademy.sameng.ecommerce_graduation_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceGraduationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
