a6d9c97c7664e3bc97823c9e186bd6d7cb355fb2
seansameng <127754501+seansameng@users.noreply.github.com>
2025-12-30 10:51:39 +0700

    add

---
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/repository/AddressRepository.java
@@ -0,0 +1,12 @@
+package com.stepacademy.sameng.ecommerce_graduation_project.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.stepacademy.sameng.ecommerce_graduation_project.models.Category;
+
+public interface AddressRepository {
+
+}
