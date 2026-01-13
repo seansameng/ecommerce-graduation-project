4f2fbfd0c9e308d3d8df092c1eec46f3a4318a49
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-13 19:57:45 +0700

    Update OrderItem.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/models/OrderItem.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/models/OrderItem.java
@@ -1,12 +1,4 @@
-package com.stepacademy.sameng.ecommerce_graduation_project.models;
 
-import jakarta.persistence.*;
-import lombok.*;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
-public class OrderItem {
 
-}
