15260c250bbbd55f6a982fc248a9239d4fcdba41
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-13 20:00:05 +0700

    Update User.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/models/User.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/models/User.java
@@ -2,6 +2,7 @@ package com.stepacademy.sameng.ecommerce_graduation_project.models;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -18,6 +19,7 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
