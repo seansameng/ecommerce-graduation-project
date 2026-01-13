ffcde88b7b8ab7b3d7b6cf5baf3343398a883e9b
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-13 19:56:14 +0700

    Update Cart.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/models/Cart.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/models/Cart.java
@@ -1,8 +1,18 @@
 package com.stepacademy.sameng.ecommerce_graduation_project.models;
 
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
