8f6a3d7dfe7dbd30f0fdadffdbdea226396e238f
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-13 19:59:31 +0700

    Update User.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/models/User.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/models/User.java
@@ -1,8 +1,5 @@
 package com.stepacademy.sameng.ecommerce_graduation_project.models;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
