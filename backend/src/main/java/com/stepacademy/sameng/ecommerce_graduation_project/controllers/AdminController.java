fb361ec241f18973a4dffc850c144f25ff77d7d3
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-15 14:25:02 +0700

    Update AdminController.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/controllers/AdminController.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/controllers/AdminController.java
@@ -1,6 +1,21 @@
 package com.stepacademy.sameng.ecommerce_graduation_project.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stepacademy.sameng.ecommerce_graduation_project.dtos.product.ProductRequest;
+import com.stepacademy.sameng.ecommerce_graduation_project.dtos.product.ProductResponse;
+import com.stepacademy.sameng.ecommerce_graduation_project.services.ProductService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/admin/products")
 public class AdminController {
     private final ProductService productService;
