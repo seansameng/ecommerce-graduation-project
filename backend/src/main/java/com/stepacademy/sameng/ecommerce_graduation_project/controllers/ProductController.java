5c43ca30670f5dc524bd015fcc30e248212aed17
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-15 14:06:01 +0700

    Add ProductController

---
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/controllers/ProductController.java
@@ -0,0 +1,30 @@
+package com.stepacademy.sameng.ecommerce_graduation_project.controllers;
+
+import com.stepacademy.sameng.ecommerce_graduation_project.dtos.product.ProductResponse;
+import com.stepacademy.sameng.ecommerce_graduation_project.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public List<ProductResponse> getAll(
+            @RequestParam(required = false) String q,
+            @RequestParam(required = false) String category) {
+        return productService.getAll(q, category);
+    }
+
+    @GetMapping("/{id}")
+    public ProductResponse getById(@PathVariable Long id) {
+        return productService.getById(id);
+    }
+}
