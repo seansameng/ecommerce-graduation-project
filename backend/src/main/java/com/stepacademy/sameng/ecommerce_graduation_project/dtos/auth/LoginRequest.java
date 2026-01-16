bde735e25aa6a0a53bac6b3b5f468f0a16688180
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-16 07:22:28 +0700

    Update LoginRequest.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/dtos/auth/LoginRequest.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/dtos/auth/LoginRequest.java
@@ -2,11 +2,10 @@ package com.stepacademy.sameng.ecommerce_graduation_project.dtos.auth;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
+
 public class LoginRequest {
 
     @NotBlank(message = "Full Name is required")
