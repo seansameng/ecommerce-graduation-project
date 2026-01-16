00dff50237eb2aa8f2979c2ea27b136e8eb725b9
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-16 07:22:49 +0700

    Update ProductService.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/services/ProductService.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/services/ProductService.java
@@ -1,12 +1,9 @@
 package com.stepacademy.sameng.ecommerce_graduation_project.services;
 
 import java.util.List;
+
 import com.stepacademy.sameng.ecommerce_graduation_project.dtos.product.ProductRequest;
 import com.stepacademy.sameng.ecommerce_graduation_project.dtos.product.ProductResponse;
-import com.stepacademy.sameng.ecommerce_graduation_project.models.Product;
-import com.stepacademy.sameng.ecommerce_graduation_project.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 public interface ProductService {
     List<ProductResponse> getAll(String query, String category);
