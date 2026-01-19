d072aa183415fb3225e6d1b9b0921591511d12db
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-19 21:12:14 +0700

    Update ProductResponse.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/dtos/product/ProductResponse.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/dtos/product/ProductResponse.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class ProductResponse {
-    private int id;
+    private Long id;
     private String name;
     private Double price;
     private Integer stock;
