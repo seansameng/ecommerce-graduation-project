a4ed1547a2958206e5ac4e28a29871d1b33e3ab1
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-19 20:31:19 +0700

    Update ProductRepository.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/repository/ProductRepository.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/repository/ProductRepository.java
@@ -2,10 +2,14 @@ package com.stepacademy.sameng.ecommerce_graduation_project.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.stepacademy.sameng.ecommerce_graduation_project.models.Product;
 
-public interface ProductRepository {
-    List<Product> findByCategory(int  categoryId);
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryId(Long categoryId);
+
+
 
     List<Product> findByNameContaining(String name);
 }
