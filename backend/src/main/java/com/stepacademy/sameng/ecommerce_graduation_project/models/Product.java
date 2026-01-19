0de3e1d5148d287f9ffdcc907d322efbe0bf0836
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-19 21:12:08 +0700

    Update Product.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/models/Product.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/models/Product.java
@@ -26,7 +26,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(nullable = false)
     private String name;
