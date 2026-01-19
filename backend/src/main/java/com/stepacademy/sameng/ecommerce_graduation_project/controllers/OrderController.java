d6ac85fb5a94d7133c4fe4ffdf449157eed08aee
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-19 21:08:21 +0700

    Create OrderController.java

---
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/controllers/OrderController.java
@@ -0,0 +1,40 @@
+package com.stepacademy.sameng.ecommerce_graduation_project.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stepacademy.sameng.ecommerce_graduation_project.dtos.order.OrderCreateRequest;
+import com.stepacademy.sameng.ecommerce_graduation_project.dtos.order.OrderResponse;
+import com.stepacademy.sameng.ecommerce_graduation_project.services.OrderService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @GetMapping
+    public List<OrderResponse> getAll() {
+        return orderService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public OrderResponse getById(@PathVariable Long id) {
+        return orderService.getById(id);
+    }
+
+    @PostMapping
+    public OrderResponse create(@Valid @RequestBody OrderCreateRequest request) {
+        return orderService.createOrder(request);
+    }
+}
