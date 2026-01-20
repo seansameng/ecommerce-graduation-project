d38bd3b5f3bd8a8369953aa990789037c4a51c03
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-20 19:59:12 +0700

    Create axios.js

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api',
+    // 8080 is the default port for backend server
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+
+api.interceptors.request.use(config => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+export default api;
