9e4ae5a69d57b11a644a4884e2de24d61ee86caa
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-20 19:59:00 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
+import Product from './pages/Product';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Product />} />
         {/* Add more routes as needed */}
       </Routes>
     </BrowserRouter>
