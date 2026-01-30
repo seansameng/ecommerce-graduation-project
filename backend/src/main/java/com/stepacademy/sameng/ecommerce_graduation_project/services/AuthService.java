50efc1e0f736f059c192baa2df63ff068c35ffa8
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-30 14:11:23 +0700

    Update AuthService.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/services/AuthService.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/services/AuthService.java
@@ -3,11 +3,12 @@ package com.stepacademy.sameng.ecommerce_graduation_project.services;
 import org.springframework.stereotype.Service;
 
 import com.stepacademy.sameng.ecommerce_graduation_project.dtos.auth.LoginRequest;
+import com.stepacademy.sameng.ecommerce_graduation_project.dtos.auth.AuthResponse;
 import com.stepacademy.sameng.ecommerce_graduation_project.dtos.auth.RegisterRequest;
 
 @Service
 public interface AuthService {
     void register(RegisterRequest request);
 
-    void login(LoginRequest request);
+    AuthResponse login(LoginRequest request);
 }
