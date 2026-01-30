1cf6464bb5bef37b82ebb6e625e8b053549376d1
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-30 14:11:02 +0700

    Create ApiException.java

---
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/exceptions/ApiException.java
@@ -0,0 +1,17 @@
+package com.stepacademy.sameng.ecommerce_graduation_project.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiException extends RuntimeException {
+
+    private final HttpStatus status;
+
+    public ApiException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
