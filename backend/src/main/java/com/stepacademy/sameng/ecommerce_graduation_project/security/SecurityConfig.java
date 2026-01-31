c1d7b04cd72f7f6c0ee21d6769c33c08ebf09307
seansameng <127754501+seansameng@users.noreply.github.com>
2026-01-31 13:54:32 +0700

    Update SecurityConfig.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/security/SecurityConfig.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -40,8 +41,7 @@ public class SecurityConfig {
         // .requestMatchers("/api/orders/**").authenticated()
         // .anyRequest().authenticated());
 
-        // http.addFilterBefore(jwtAuthFilter,
-        // UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
