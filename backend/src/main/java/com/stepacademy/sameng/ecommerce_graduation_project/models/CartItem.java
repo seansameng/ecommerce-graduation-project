23bd4f4a2be20f6607aeabb5eb25f83703f604e0
seansameng <127754501+seansameng@users.noreply.github.com>
2026-02-01 10:49:36 +0700

    Create CartItem.java

---
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/models/CartItem.java
@@ -0,0 +1,88 @@
+package com.stepacademy.sameng.ecommerce_graduation_project.models;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "cart_items", uniqueConstraints = @UniqueConstraint(columnNames = { "cart_id", "product_id" }))
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+    // add unitPriceSnapshot to CartItem to preserve price at time of addition to
+    // cart
+    @Column(nullable = false)
+    private BigDecimal unitPriceSnapshot;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    // getters/setters
+    public Long getId() {
+        return id;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getUnitPriceSnapshot() {
+        return unitPriceSnapshot;
+    }
+
+    public void setUnitPriceSnapshot(BigDecimal unitPriceSnapshot) {
+        this.unitPriceSnapshot = unitPriceSnapshot;
+    }
+}
