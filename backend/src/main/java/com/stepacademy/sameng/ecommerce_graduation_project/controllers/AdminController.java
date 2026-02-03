6230ed5600b103f009f466e460ddc20f2ac03876
seansameng <127754501+seansameng@users.noreply.github.com>
2026-02-03 09:48:19 +0700

    Update AdminController.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/controllers/AdminController.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/controllers/AdminController.java
@@ -1,17 +1,22 @@
 package com.stepacademy.sameng.ecommerce_graduation_project.controllers;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stepacademy.sameng.ecommerce_graduation_project.dtos.product.ProductRequest;
 import com.stepacademy.sameng.ecommerce_graduation_project.dtos.product.ProductResponse;
 import com.stepacademy.sameng.ecommerce_graduation_project.services.ProductService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -20,13 +25,20 @@ import lombok.RequiredArgsConstructor;
 public class AdminController {
     private final ProductService productService;
 
+    @GetMapping
+    public List<ProductResponse> getAll(
+            @RequestParam(required = false) String q,
+            @RequestParam(required = false) String category) {
+        return productService.getAll(q, category);
+    }
+
     @PostMapping
-    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+    public ProductResponse createProduct(@Valid @RequestBody ProductRequest productRequest) {
         return productService.createProduct(productRequest);
     }
 
     @PutMapping("/{id}")
-    public ProductResponse updateProduct(@PathVariable Long id, @RequestBody ProductRequest productRequest) {
+    public ProductResponse updateProduct(@PathVariable Long id, @Valid @RequestBody ProductRequest productRequest) {
         return productService.updateProduct(id, productRequest);
     }
 
