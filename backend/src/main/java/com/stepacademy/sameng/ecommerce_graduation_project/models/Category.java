66fc4dd54fe94c155d55c0015d3f6ede280582b9
seansameng <127754501+seansameng@users.noreply.github.com>
2026-02-03 09:48:17 +0700

    Update Category.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/models/Category.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/models/Category.java
@@ -34,12 +34,6 @@ public class Category {
     @Column(nullable = false, unique = true)
     private String name;
 
-    @Column(columnDefinition = "TEXT")
-    private String description;
-
-    @Column(name = "image_url")
-    private String imageUrl;
-
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
     @Builder.Default
     private List<Product> products = new ArrayList<>();
