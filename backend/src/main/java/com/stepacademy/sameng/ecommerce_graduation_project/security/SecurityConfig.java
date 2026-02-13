fe638da9e760881f8a39ba4705d5f744d8827ee1
seansameng <127754501+seansameng@users.noreply.github.com>
2026-02-13 13:27:54 +0700

    Update SecurityConfig.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/security/SecurityConfig.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                 .requestMatchers("/error").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/categories/**").permitAll()
                 .requestMatchers("/api/admin/**").hasRole("ADMIN")
                 .requestMatchers("/api/orders/**").authenticated()
                 .anyRequest().authenticated());
