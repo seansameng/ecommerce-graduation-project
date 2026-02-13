37311f873f3ee9e223b0f1b4452b1c1ad81183ca
seansameng <127754501+seansameng@users.noreply.github.com>
2026-02-13 19:06:31 +0700

    Update CategoryRequest.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/dtos/category/CategoryRequest.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/dtos/category/CategoryRequest.java
@@ -2,9 +2,13 @@ package com.stepacademy.sameng.ecommerce_graduation_project.dtos.category;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonAlias;
 
 @Data
 public class CategoryRequest {
     @NotBlank(message = "Name is required")
     private String name;
+
+    @JsonAlias("image_url")
+    private String imageUrl;
 }
