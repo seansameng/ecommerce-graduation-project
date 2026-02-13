661940dc18cd825a8b0b69e96b0cb858069bf0c4
seansameng <127754501+seansameng@users.noreply.github.com>
2026-02-13 13:27:02 +0700

    Create CategoryResponse.java

---
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/dtos/category/CategoryResponse.java
@@ -0,0 +1,13 @@
+package com.stepacademy.sameng.ecommerce_graduation_project.dtos.category;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class CategoryResponse {
+    private Long id;
+    private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
