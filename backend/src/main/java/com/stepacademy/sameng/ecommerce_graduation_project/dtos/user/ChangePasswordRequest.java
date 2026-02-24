7027733251b80026e37f9a9c1b0ad4e080493d35
seansameng <127754501+seansameng@users.noreply.github.com>
2026-02-24 16:18:09 +0700

    Create ChangePasswordRequest.java

---
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/dtos/user/ChangePasswordRequest.java
@@ -0,0 +1,27 @@
+package com.stepacademy.sameng.ecommerce_graduation_project.dtos.user;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class ChangePasswordRequest {
+    @NotBlank(message = "Current password is required")
+    private String currentPassword;
+
+    @NotBlank(message = "New password is required")
+    private String newPassword;
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
