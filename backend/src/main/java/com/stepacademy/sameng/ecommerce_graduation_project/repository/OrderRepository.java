7a059e639f2e103b70ea9c8dd0f46f13734e1348
seansameng <127754501+seansameng@users.noreply.github.com>
2026-02-24 16:17:38 +0700

    Update OrderRepository.java

--- backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/repository/OrderRepository.java
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/repository/OrderRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.stepacademy.sameng.ecommerce_graduation_project.models.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    java.util.List<Order> findByUserIdOrderByCreatedAtDesc(Long userId);
 }
