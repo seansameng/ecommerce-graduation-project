8866f2169c43b5cffc762dad41d6370f329fc020
seansameng <127754501+seansameng@users.noreply.github.com>
2026-02-24 16:19:18 +0700

    Create UpdateProfileRequest.java

---
+++ backend/src/main/java/com/stepacademy/sameng/ecommerce_graduation_project/dtos/user/UpdateProfileRequest.java
@@ -0,0 +1,25 @@
+package com.stepacademy.sameng.ecommerce_graduation_project.dtos.user;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class UpdateProfileRequest {
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+    private String phone;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
