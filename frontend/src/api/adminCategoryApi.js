e98a2c8a3b0274d34331b12904f81449839caaae
seansameng <127754501+seansameng@users.noreply.github.com>
2026-02-27 11:04:12 +0700

    Update adminCategoryApi.js

--- frontend/src/api/adminCategoryApi.js
+++ frontend/src/api/adminCategoryApi.js
@@ -1,6 +1,6 @@
-import api from "./api";
+import api from "./axios";
 
-export const getCategories = () => api.get("/api/admin/categories");
-export const createCategory = (data) => api.post("/api/admin/categories", data);
-export const updateCategory = (id, data) => api.put(`/api/admin/categories/${id}`, data);
-export const deleteCategoryById = (id) => api.delete(`/api/admin/categories/${id}`);
+export const getCategories = () => api.get("/admin/categories");
+export const createCategory = (data) => api.post("/admin/categories", data);
+export const updateCategory = (id, data) => api.put(`/admin/categories/${id}`, data);
+export const deleteCategoryById = (id) => api.delete(`/admin/categories/${id}`);
